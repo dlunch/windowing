3cfd1cc5260f2086aa35bfccb2edac32fefad4bc
Inseok Lee <git@dlun.ch>
2023-01-03 20:55:02 +0900

    Split window into a module

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,6 @@
 mod event;
 mod platform;
+mod window;
 
-use platform::WindowImpl;
-
-pub struct Window {
-    window_impl: WindowImpl,
-}
-
-impl Window {
-    pub fn new(width: i32, height: i32, title: &str) -> Self {
-        let window_impl = WindowImpl::new(width, height, title);
-
-        Self { window_impl }
-    }
-
-    pub fn run(self) {
-        self.window_impl.run()
-    }
-}
+pub use event::Event;
+pub use window::Window;
---
+++ src/window.rs
@@ -0,0 +1,17 @@
+use crate::platform::WindowImpl;
+
+pub struct Window {
+    window_impl: WindowImpl,
+}
+
+impl Window {
+    pub fn new(width: i32, height: i32, title: &str) -> Self {
+        let window_impl = WindowImpl::new(width, height, title);
+
+        Self { window_impl }
+    }
+
+    pub fn run(self) {
+        self.window_impl.run()
+    }
+}
