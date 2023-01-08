58a005fa7cb0fdffadab6e9056d202b156188ba7
Inseok Lee <git@dlun.ch>
2023-01-08 09:47:52 +0900

    Remove sample redraw on wayland

--- src/platform/wayland.rs
+++ src/platform/wayland.rs
@@ -1,16 +1,11 @@
 use alloc::vec::Vec;
-use core::{cmp::min, iter};
+use core::iter;
 use std::io;
 
 use raw_window_handle::{RawWindowHandle, WaylandWindowHandle};
 use smithay_client_toolkit::{
-    environment::Environment,
     new_default_environment,
-    reexports::client::{
-        protocol::{wl_shm, wl_surface},
-        EventQueue,
-    },
-    shm::AutoMemPool,
+    reexports::client::EventQueue,
     window::{Event as WEvent, FallbackFrame, Window},
 };
 
@@ -19,7 +14,6 @@ use crate::Event;
 smithay_client_toolkit::default_environment!(Wayland, desktop);
 
 pub struct WindowImpl {
-    env: Environment<Wayland>,
     window: Window<FallbackFrame>,
     queue: EventQueue,
     dimensions: (u32, u32),
@@ -42,17 +36,10 @@ impl WindowImpl {
 
         window.set_title(title.into());
 
-        Self {
-            env,
-            window,
-            queue,
-            dimensions,
-        }
+        Self { window, queue, dimensions }
     }
 
     pub async fn next_events(&mut self, wait: bool) -> impl Iterator<Item = Event> {
-        let mut pool = self.env.create_auto_pool().expect("Failed to create a memory pool !");
-
         let mut events = Vec::<WEvent>::new();
 
         if wait {
@@ -87,7 +74,6 @@ impl WindowImpl {
                     println!("Window states: {states:?}");
 
                     self.window.refresh();
-                    redraw(&mut pool, self.window.surface(), self.dimensions).expect("Failed to draw");
                 }
             }
         }
@@ -102,29 +88,4 @@ impl WindowImpl {
 
         RawWindowHandle::Wayland(window_handle)
     }
-}
-
-#[allow(clippy::many_single_char_names)]
-fn redraw(pool: &mut AutoMemPool, surface: &wl_surface::WlSurface, (buf_x, buf_y): (u32, u32)) -> Result<(), ::std::io::Error> {
-    let (canvas, new_buffer) = pool.buffer(buf_x as i32, buf_y as i32, 4 * buf_x as i32, wl_shm::Format::Argb8888)?;
-    for (i, dst_pixel) in canvas.chunks_exact_mut(4).enumerate() {
-        let x = i as u32 % buf_x;
-        let y = i as u32 / buf_x;
-        let r: u32 = min(((buf_x - x) * 0xFF) / buf_x, ((buf_y - y) * 0xFF) / buf_y);
-        let g: u32 = min((x * 0xFF) / buf_x, ((buf_y - y) * 0xFF) / buf_y);
-        let b: u32 = min(((buf_x - x) * 0xFF) / buf_x, (y * 0xFF) / buf_y);
-        let pixel: [u8; 4] = ((0xFF << 24) + (r << 16) + (g << 8) + b).to_ne_bytes();
-        dst_pixel[0] = pixel[0];
-        dst_pixel[1] = pixel[1];
-        dst_pixel[2] = pixel[2];
-        dst_pixel[3] = pixel[3];
-    }
-    surface.attach(Some(&new_buffer), 0, 0);
-    if surface.as_ref().version() >= 4 {
-        surface.damage_buffer(0, 0, buf_x as i32, buf_y as i32);
-    } else {
-        surface.damage(0, 0, buf_x as i32, buf_y as i32);
-    }
-    surface.commit();
-    Ok(())
 }
